530f2a313e32d42fae7f90517e8cfe54a4953424
Klim Savanovich <klimsava96@mail.ru>
2020-09-29 21:13:25 +0300

    init: start basic-js task and add carbon-dating, count-cats

--- src/carbon-dating.js
+++ src/carbon-dating.js
@@ -3,7 +3,13 @@ const CustomError = require("../extensions/custom-error");
 const MODERN_ACTIVITY= 15; 
 const HALF_LIFE_PERIOD= 5730;
 
-module.exports = function dateSample(/* sampleActivity */) {
+module.exports = function dateSample(sampleActivity) {
+  if (typeof(sampleActivity) == 'string' && parseFloat(sampleActivity) > 0 && parseFloat(sampleActivity) <= 15) {
+    return Math.ceil((Math.log(MODERN_ACTIVITY / parseFloat(sampleActivity))) / 0.693 / HALF_LIFE_PERIOD);
+  } else {
+    return false;
+  }
+
   throw new CustomError('Not implemented');
   // remove line with error and write your code here
 };
