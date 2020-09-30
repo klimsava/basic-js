88e7fe830140224e0e4c2da855114bcc229d40ef
Klim Savanovich <klimsava96@mail.ru>
2020-09-30 09:02:50 +0300

    feat: add dream-team task

--- src/dream-team.js
+++ src/dream-team.js
@@ -1,6 +1,17 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function createDreamTeam(/* members */) {
+module.exports = function createDreamTeam(arr) {
+  let result = [];
+  if (!Array.isArray(arr)) return false;
+
+  for (let i = 0; i < arr.length; i++) {
+    if (typeof arr[i] === 'string') {
+      result.push(arr[i].trim().slice(0, 1).toUpperCase());
+    }
+  }
+
+  return result.sort().join('');
+
   throw new CustomError('Not implemented');
   // remove line with error and write your code here
 };
