6945dc48d1b0e46aba6ef7c19176a452043dd896
Klim Savanovich <klimsava96@mail.ru>
2020-10-02 18:58:32 +0300

    feat: add simple-chain and what-season tasks

--- src/simple-chain.js
+++ src/simple-chain.js
@@ -1,26 +1,32 @@
 const CustomError = require("../extensions/custom-error");
 
-const chainMaker = {
+module.exports = chainMaker = {
+  result: [],
   getLength() {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
+    return this.result.length;
   },
   addLink(value) {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
+    this.result.push(value);
+
+    return this;
   },
   removeLink(position) {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
+    if(position < 1 || position > this.getLength() || typeof position !== 'number'){
+      this.result = [];
+      throw Error('No position');
+    }
+
+    this.result.splice(position-1, 1);
+
+    return this;
   },
   reverseChain() {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
+    this.arr.reverse();
+    return this;
   },
   finishChain() {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
+    let finalArr = this.arr.map(item => `( ${item} )`);
+    this.arr = [];
+    return finalArr.join('~~');
   }
 };
-
-module.exports = chainMaker;
